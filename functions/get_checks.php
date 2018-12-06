0b27369df1f048db8a67cbe22cc89046189b3264
RCDaddy <tupoar@remixcity.co.uk>
2018-12-06 15:01:40 +0000

    Create get_checks.php
    Adding listing feature

---
+++ functions/get_checks.php
@@ -0,0 +1,26 @@
+<?php
+// Copyright (C) 2015 Remy van Elst
+ // This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+ // This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+ // You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ function get_domain_checks() {
+    global $current_domain;
+    global $current_link;
+    global $check_file;
+     $file = file_get_contents($check_file);
+    if ($file === FALSE) {
+        return null;
+    }
+    $json_a = json_decode($file, true);
+    if ($json_a === null && json_last_error() !== JSON_ERROR_NONE) {
+        return null;
+    }
+    return $json_a;
+} 
